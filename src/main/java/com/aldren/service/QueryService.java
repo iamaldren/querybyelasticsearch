8ec52c39ba7d5ceb53d8440a993ed538e7271cff
iamaldrew <aldrenbobiles@gmail.com>
2019-09-12 23:49:16 +0800

    Added get function

--- src/main/java/com/aldren/service/QueryService.java
+++ src/main/java/com/aldren/service/QueryService.java
@@ -1,6 +1,5 @@
 package com.aldren.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 
@@ -11,5 +10,7 @@ public interface QueryService<T> {
     ObjectMapper mapper = new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT);
 
     String createData(T data) throws IOException;
+
+    T getData(int id) throws IOException;
 
 }
--- src/main/java/com/aldren/service/impl/PokemonQueryService.java
+++ src/main/java/com/aldren/service/impl/PokemonQueryService.java
@@ -2,8 +2,8 @@ package com.aldren.service.impl;
 
 import com.aldren.model.Pokemon;
 import com.aldren.service.QueryService;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import org.elasticsearch.action.ActionListener;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.RequestOptions;
@@ -14,11 +14,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.util.Map;
 
 @Service
 public class PokemonQueryService implements QueryService<Pokemon> {
 
     private static final String INDEX = "pokemon";
+    private static final String TYPE = "pokedex";
 
     @Autowired
     private RestHighLevelClient client;
@@ -26,7 +28,7 @@ public class PokemonQueryService implements QueryService<Pokemon> {
     @Override
     public String createData(Pokemon data) throws IOException {
         IndexRequest request = new IndexRequest(INDEX)
-                .type("pokedex")
+                .type(TYPE)
                 .id(String.valueOf(data.getIndex()));
 
         String pokemonJson = mapper.writeValueAsString(data);
@@ -37,5 +39,15 @@ public class PokemonQueryService implements QueryService<Pokemon> {
         IndexResponse response = client.index(request, RequestOptions.DEFAULT);
 
         return response.getResult().name();
+    }
+
+    @Override
+    public Pokemon getData(int id) throws IOException {
+        GetRequest request = new GetRequest(INDEX).type(TYPE).id(String.valueOf(id));
+
+        GetResponse response = client.get(request, RequestOptions.DEFAULT);
+        Map<String, Object> result = response.getSource();
+
+        return mapper.convertValue(result, Pokemon.class);
     }
 }
